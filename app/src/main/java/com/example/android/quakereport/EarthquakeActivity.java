2834b2ae7a94daa5d0c9f9e56be5debd8eb11047
mobilotest <mobilotest@gmail.com>
2020-05-11 09:13:59 -0700

    updated version, ready to continue

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -16,9 +16,10 @@
 package com.example.android.quakereport;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
